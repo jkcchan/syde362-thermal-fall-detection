ebe478e64caf843cacbbd8daca5107977267cd8b
Jacob Chan <jacob.chan5@gmail.com>
2018-03-16 12:05:06 -0400

    interpret as image, interpret as text file

--- interpret_bin_file.py
+++ interpret_bin_file.py
@@ -1,4 +1,7 @@
-def interpret_bin_file(filename, target):
+"""
+interprets bin file and saves a PNG in target directory
+"""
+def interpret_bin_file_as_img(filename, target):
     file = open(filename)
     hex_list = []
     for line in file:
@@ -16,3 +19,23 @@ def interpret_bin_file(filename, target):
         data += chr(pixels[i]) + chr(pixels[i]) + chr(pixels[i])
     im = Image.frombytes("RGB", (80,60), data)
     im.save(target, "PNG")
+"""
+interprets bin file and saves a txt in target directory
+"""
+def interpret_bin_file_as_txt_file(filename, target):
+    file = open(filename)
+    hex_list = []
+    for line in file:
+        hex_list.extend((''.join( [ "%02X " % ord( x ) for x in line ] ).strip()).split(" "))
+    new_hex_list = []
+    for i, v in enumerate(hex_list):
+        if i%2==0:
+            new_hex_list.append(int(hex_list[i]+hex_list[i+1], 16))
+    txt_file = ""
+    for i in range(0,60):
+        for j in range(0, 80):
+            txt_file+=str(new_hex_list[i*j+2])+" "
+        txt_file+="\n "
+    text_file = open(target, "w")
+    text_file.write(txt_file)
+    text_file.close()
