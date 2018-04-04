b23afa5515b30fca193d4eaf58d95ee0b3c360da
Jacob Chan <jkcchan@users.noreply.github.com>
2018-04-04 11:22:44 -0400

    Clean (#1)
    * new setup branching
    
    * WIP
    
    * removes junk
    
    * 22-52: Sammie lying down N=30
    
    * moving dataset-1
    
    * jacob lying down:
    
    * dataset-2 is Jacob on the ground
    
    * edgecase lying down heated kettle as noise
    
    * hassan sitting
    
    * dataset-3 is lying down with obstructions (kettle/phone). dataset-4 is hassan sitting
    
    * Sammie standing
    
    * dataset-5 is Sammie standing
    
    * empty room and odd cases
    
    * dataset-6 is empty room and edge cases
    
    * sitting 2nd
    
    * dataset-7 is sitting - extended. some on floor
    
    * add bin interpreter
    
    * interpret as image, interpret as text file
    
    * change code a little
    
    * numpy arrays
    
    * add notebooks
    
    * hu's moments
    
    * cv2CCA
    
    * hu moment summaries
    
    * Added csv file of different features. Probably Hassan needs to use his classifier, as the features don't seem to fit along the line. It's more like a classification problem
    
    * adds SYDE372 lab1
    
    * first attempt at using med with data
    
    * I created a test dataset. My regression model is somewhat okay. Out of 80 samples, there are 39 lying, and others are sitting or standing. The regression model with 2 variates, gives only 1 false_positive, but 9 false_negatives (meaning someone fell, but model didn't recognise). I used a standard prediction treshold of 0.5)
    
    * new features
    
    * better code
    
    * even better code
    
    * rotated box function
    
    * 2 new csv files
    
    * added other classification methods
    
    * add best classifier
    
    * rotbox csv files
    
    * upload knn classifier
    
    * final notebook
    
    * for report
    
    * py file
    
    * edits
    
    * add min py file
    
    * edit
    
    * for rpi
    
    * train
    
    * adds ez_server
    
    * dataset folder
    
    * with new score function
    
    * oops
    
    * Variance Breakdown
    
    * new classifier, svm
    
    * tightvncserver autoboot
    
    * svm classifier with proba
    
    * outputs red boxes
    
    * clean

---
+++ fall_detection.py
@@ -0,0 +1,128 @@
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import numpy as np
+import cv2
+
+def get_CCA_from_image(image, threshold = 115, show_image=False):
+    img = cv2.imread(image,0)
+    h,w = img.shape[:2]
+    if show_image:
+        fig,ax = plt.subplots(1)
+
+    L = measure.label(img)
+
+    ret, thresh = cv2.threshold(img,threshold,255,cv2.THRESH_BINARY_INV)
+    
+    output = cv2.connectedComponentsWithStats(thresh, 8, cv2.CV_32S)
+    num_labels = output[0]
+    labels = output[1]
+    stats = output[2]
+    centroids = output[3]
+
+    max_area_index=-1
+    for i in range(len(stats)):
+        if stats[i][0]==0 and stats[i][1]==0:
+            continue
+        if show_image:
+            ax.add_patch(
+                patches.Rectangle(
+                    (stats[i][0], stats[i][1]),
+                    stats[i][2],
+                    stats[i][3],
+                    fill=False,      # remove background
+                    color='red'
+                )
+            )
+        if max_area_index==-1:
+            max_area_index=i
+        elif stats[i][4] > stats[max_area_index][4]:
+            max_area_index=i
+            
+    if show_image:
+        ax.imshow(thresh, cmap='gray')
+        plt.show()
+    chosen_box = thresh[stats[max_area_index][1]:stats[max_area_index][1]+stats[max_area_index][3],stats[max_area_index][0]:stats[max_area_index][0]+stats[max_area_index][2]]
+    return stats[max_area_index], chosen_box, thresh, img
+
+
+def get_best_threshold(img_filename, start=30, max_steps =100, alpha=1):
+    min_ratio = float("inf")
+    index = -1
+    final_chosen_box = None
+    final_stats = []
+    final_img = None
+    final_thresh = None
+    thresholds = np.arange(start,start+max_steps,alpha)
+    for i in thresholds:
+        stats, chosen_box, thresh, img = get_CCA_from_image(img_filename, i, show_image=False)
+        ratio = get_ratio_of_image(chosen_box)
+        if ratio < min_ratio:
+            min_ratio = ratio
+            index = i
+            final_chosen_box = chosen_box
+            final_stats = stats
+            final_thresh = thresh
+            final_img = img
+    return index, final_chosen_box, final_stats, final_thresh, final_img
+
+def get_ratio_of_image(img):
+    #ratio of white to black
+    a=cv2.countNonZero(img)
+    h,w = img.shape[:2]
+#     print type(float(a))
+#     print (float(h)*float(w))
+    return float(float(a)/(float(h)*float(w)))
+
+def get_rotated_box(image_filename, output = False, max_area = 2700):
+    images = []
+    threshold, chosen_box, stats, thresh, img = get_best_threshold(image_filename, start=40)
+    im2,contours,hierarchy = cv2.findContours(thresh, 1, 2)
+    areas = []
+    for cont in contours:
+        areas.append(cv2.contourArea(cont))
+    areas = np.array(areas)
+    cont = contours[np.argmax(areas[areas < max_area])]
+    rect = cv2.minAreaRect(cont)
+    if output:
+        box = cv2.boxPoints(rect)
+        box = np.int0(box)
+        images.append(thresh)
+        a = cv2.drawContours(thresh,[box],0,(255, 255,0),2)
+        plt.imshow(thresh, cmap="gray")
+        plt.show()
+    return rect[1][0]*rect[1][1], max(rect[1][0]/rect[1][1],rect[1][1]/rect[1][0])
+
+def get_features(filename, output=False):
+    index, final_chosen_box, final_stats, final_thresh, final_img = get_best_threshold(filename)
+    features = []
+    area = final_stats[2]*final_stats[3]
+    features.append(area)
+    bb_ratio =max((float(final_stats[2])/float(final_stats[3])),(float(final_stats[3])/float(final_stats[2])))
+    features.append(bb_ratio)
+    hu_moments = cv2.HuMoments(cv2.moments(final_chosen_box))
+    features.extend(hu_moments.T[0].tolist())
+    variance = np.var(final_chosen_box)
+    features.append(variance)
+    if output:
+        print "threshold used: " + str(index)
+        plt.imshow(final_chosen_box, cmap="gray")
+        plt.show()
+        print "area: "+str(area)
+        print "bb_ratio: "+ str(bb_ratio)
+        print "hu_moments: "+np.array2string(hu_moments.T[0])
+        print "variance: "+str(variance)
+    return features
+    
+def scale_features(features):
+    if not scl:
+        return
+    if len(features) != (scl.min_).shape[0]:
+        return
+    return scl.transform([features])
+
+def classify_with_scaled_features(scaled_features, probabilities = False):
+    if not clf:
+        return
+    if probabilities:
+        return clf.predict(scaled_features), clf.predict_proba(scaled_features)
+    return clf.predict(scaled_features)
